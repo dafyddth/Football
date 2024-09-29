3ffbc5696e313d34a854e032693c8a5a8b61a5fc
dafyddth <dafyddth@googlemail.com>
2024-09-29 05:34:52 +0100

    Need to get match fdate into forebet

--- Database.py
+++ Database.py
@@ -30,12 +30,15 @@ def insert_betfair_odds(bf_event_id, bf_market_id, odds_date, bf_home_back_odds,
     conn.commit()
     conn.close()
 
-def insert_forebet_odds(odds_date, home_odds, draw_odds, away_odds, session_id, home_team, away_team, match_date, match_time):
-    sql = "INSERT INTO Forebetodds(OddsDate, FBHomeBackOdds, FBDrawBackOdds, FBAwayBackOdds, SessionID, HomeTeam, AwayTeam, MatchDate, MatchTime)"\
+
+def insert_forebet_odds(odds_date, home_odds, draw_odds, away_odds, session_id, home_team, away_team, match_date,
+                        match_time):
+    sql = "INSERT INTO Forebetodds(OddsDate, FBHomeBackOdds, FBDrawBackOdds, FBAwayBackOdds, SessionID, HomeTeam, AwayTeam, MatchDate, MatchTime)" \
           " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
     conn = sqlite3.connect('Football.db')
     cursor = conn.cursor()
-    cursor.execute(sql, (odds_date, home_odds, draw_odds, away_odds, session_id, home_team, away_team, match_date, match_time))
+    cursor.execute(sql, (
+    odds_date, home_odds, draw_odds, away_odds, session_id, home_team, away_team, match_date, match_time))
     conn.commit()
     conn.close()
 
@@ -49,5 +52,13 @@ def correct_team_names():
     sql = ("UPDATE Market SET AwayTeam = tblTeamDictionary.CorrectName FROM "
            "tblTeamDictionary WHERE Market.AwayTeam = tblTeamDictionary.originalName")
     cursor.execute(sql)
+    conn.commit()
+    sql = ("UPDATE ForebetOdds SET HomeTeam = tblTeamDictionary.CorrectName FROM "
+           "tblTeamDictionary WHERE ForebetOdds.HomeTeam = tblTeamDictionary.originalName ")
 
+    cursor.execute(sql)
+    conn.commit()
+    sql = ("UPDATE ForebetOdds SET AwayTeam = tblTeamDictionary.CorrectName FROM "
+           "tblTeamDictionary WHERE ForebetOdds.AwayTeam = tblTeamDictionary.originalName")
+    cursor.execute(sql)
     conn.commit()
