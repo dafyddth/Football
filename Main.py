0370c0bf490dacedeef7807f4f13130bb117a8cd
dafyddth <dafyddth@googlemail.com>
2025-03-09 18:53:53 +0000

    Added more to SQLite

--- Main.py
+++ Main.py
@@ -2,15 +2,17 @@ import Betfair
 import Scraper
 import datetime
 import Database as db
+import time
 
-
-now = datetime.datetime.now()
-current_date_time = now.strftime("%Y-%m-%d %H:%M:%S")
-session_id = int(now.strftime("%Y%m%d%H%M%S"))
-Betfair.get_betfair_data(current_date_time, session_id)
-Scraper.get_predicted_odds(current_date_time, session_id,'forebet')
-Scraper.get_predicted_odds(current_date_time, session_id,'predictz')
-db.update_marketids()
-print(session_id)
-print(current_date_time)
-print("Main Finished")
+while True:
+    now = datetime.datetime.now()
+    current_date_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    session_id = int(now.strftime("%Y%m%d%H%M%S"))
+    Betfair.get_betfair_data(current_date_time, session_id)
+    Scraper.get_predicted_odds(current_date_time, session_id,'forebet')
+    Scraper.get_predicted_odds(current_date_time, session_id,'predictz')
+    db.update_marketids()
+    print(session_id)
+    print(current_date_time)
+    print("Main Finished")
+    time.sleep(21600)
